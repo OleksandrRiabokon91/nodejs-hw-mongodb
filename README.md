9a8251e8c4db3425613e125836950bac637a3f1e
Oleksandr Riabokon <riabokon.oleksandr@gmail.com>
2025-09-18 17:45:28 +0300

    First commit: successful MongoDB connection

---
+++ README.md
@@ -0,0 +1,23 @@
+Developer: Oleksandr Riabokon email: riabokon.oleksandr@gmail.com
+
+List to install:
+
+npm init -y
+npm i nodemon
+npm install --save-dev nodemon
+
+npm init @eslint/config@latest
+
+<!--  -->
+
+npm install express
+
+npm install pino-http
+
+npm i --save-dev pino-pretty
+
+npm i cors
+
+npm install dotenv
+
+npm install mongoose
--- src/server.js
+++ src/server.js
@@ -1,53 +1,62 @@
+// src/server.js
+
 import express from 'express';
 import pino from 'pino-http';
+import cors from 'cors';
+import { getEnvVar } from './utils/getEnvVar.js';
 
-const app = express();
-const PORT = 3000;
+const PORT = Number(getEnvVar('PORT', '3000'));
 
-// pino
+export const setupServer = () => {
+  const app = express();
 
-app.use(
-  pino({
-    transport: {
-      target: 'pino-pretty',
-    },
-  }),
-);
+  // Вбудований у express middleware для обробки (парсингу) JSON-даних у запитах
+  // наприклад, у запитах POST або PATCH
+  app.use(express.json());
+  app.use(cors());
 
-// Middleware для логування часу запиту
-app.use((req, res, next) => {
-  console.log(`Time: ${new Date().toLocaleString()}`);
-  next();
-});
+  // pino
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
 
-// Вбудований у express middleware для обробки (парсингу) JSON-даних у запитах
-// наприклад, у запитах POST або PATCH
-app.use(express.json());
-// Маршрут для обробки GET-запитів на '/'
-
-app.get('/', (req, res) => {
-  res.json({
-    message: 'Hello world!',
+  // Маршрут для обробки GET-запитів на '/'
+  app.get('/', (req, res) => {
+    res.json({
+      message: 'Hello world!',
+    });
   });
-});
 
-// Middleware для обробки помилки неісн. маршрутів
-
-app.use((req, res, next) => {
-  res.status(404).json({
-    message: 'Not found',
+  // Middleware для обробки помилки неісн. маршрутів
+  app.use((req, res, next) => {
+    res.status(404).json({
+      message: 'Not found',
+    });
   });
-});
 
-// Middleware для обробки помилок (приймає 4 аргументи)
-
-app.use((err, req, res, next) => {
-  res.status(500).json({
-    message: 'Something went wrong',
-    error: err.message,
+  // Middleware для обробки помилок (приймає 4 аргументи)
+  app.use((err, req, res, next) => {
+    res.status(500).json({
+      message: 'Something went wrong',
+      error: err.message,
+    });
   });
-});
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+  app.listen(PORT, (error) => {
+    if (error) {
+      throw error;
+    }
+    console.log(`Server is running on port ${PORT}`);
+  });
+  // ? ========================================================
+
+  //* Middleware для логування часу запиту
+  //   app.use((req, res, next) => {
+  //     console.log(`Time: ${new Date().toLocaleString()}`);
+  //     next();
+  //   });
+};
