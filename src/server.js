8dc92e818df2152ec3d3e442c5825aa0615c6959
Oleksandr Riabokon <riabokon.oleksandr@gmail.com>
2025-09-18 21:41:07 +0300

    lokal tests sesuccessfully

--- src/server.js
+++ src/server.js
@@ -4,18 +4,16 @@ import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
 import { getEnvVar } from './utils/getEnvVar.js';
+import { getAllContacts, getContactById } from './services/contacts.js';
 
 const PORT = Number(getEnvVar('PORT', '3000'));
 
 export const setupServer = () => {
   const app = express();
 
-  // Вбудований у express middleware для обробки (парсингу) JSON-даних у запитах
-  // наприклад, у запитах POST або PATCH
   app.use(express.json());
   app.use(cors());
 
-  // pino
   app.use(
     pino({
       transport: {
@@ -24,21 +22,12 @@ export const setupServer = () => {
     }),
   );
 
-  // Маршрут для обробки GET-запитів на '/'
   app.get('/', (req, res) => {
     res.json({
       message: 'Hello world!',
     });
   });
 
-  // Middleware для обробки помилки неісн. маршрутів
-  app.use((req, res, next) => {
-    res.status(404).json({
-      message: 'Not found',
-    });
-  });
-
-  // Middleware для обробки помилок (приймає 4 аргументи)
   app.use((err, req, res, next) => {
     res.status(500).json({
       message: 'Something went wrong',
@@ -52,11 +41,36 @@ export const setupServer = () => {
     }
     console.log(`Server is running on port ${PORT}`);
   });
-  // ? ========================================================
 
-  //* Middleware для логування часу запиту
-  //   app.use((req, res, next) => {
-  //     console.log(`Time: ${new Date().toLocaleString()}`);
-  //     next();
-  //   });
+  app.get('/contacts', async (req, res) => {
+    const contacts = await getAllContacts();
+
+    res.status(200).json({
+      data: contacts,
+    });
+  });
+
+  app.get('/contacts/:contactId', async (req, res, next) => {
+    const { contactId } = req.params;
+    const contact = await getContactById(contactId);
+
+    if (!contact) {
+      res.status(404).json({
+        message: 'Contact not found',
+      });
+      return;
+    }
+
+    res.status(200).json({
+      status: 200,
+      message: `Successfully found contact with id ${contactId}!`,
+      data: contact,
+    });
+  });
+
+  app.use((req, res, next) => {
+    res.status(404).json({
+      message: 'Not found',
+    });
+  });
 };
