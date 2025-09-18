63e896c8b3412c665b7245ee143fd82861923046
Oleksandr Riabokon <riabokon.oleksandr@gmail.com>
2025-09-18 21:58:39 +0300

    remove comments

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -1,6 +1,3 @@
-// src/db/initMongoDB.js
-//! import 'dotenv/config' из примера лекции - подключить какможно раньше в коде dotenv ?
-
 import mongoose from 'mongoose';
 
 import { getEnvVar } from '../utils/getEnvVar.js';
@@ -21,34 +18,3 @@ export const initMongoConnection = async () => {
     throw e;
   }
 };
-// ! код из примера с сайта монгоДБ
-
-// При вдалому підключенні до вашої базі в mongodb виводьте в
-// консоль рядок "Mongo connection successfully established!".
-
-// const { MongoClient, ServerApiVersion } = require('mongodb');
-// const uri =
-//   'mongodb+srv://riabokonoleksandr_db_user:4MGEEpZYwwjnLMej@cluster0.ap8qlpi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
-// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-// const client = new MongoClient(uri, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   },
-// });
-// async function run() {
-//   try {
-//     // Connect the client to the server	(optional starting in v4.7)
-//     await client.connect();
-//     // Send a ping to confirm a successful connection
-//     await client.db('admin').command({ ping: 1 });
-//     console.log(
-//       'Pinged your deployment. You successfully connected to MongoDB!',
-//     );
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     await client.close();
-//   }
-// }
-// run().catch(console.dir);
