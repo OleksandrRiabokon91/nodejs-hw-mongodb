e4002b990260865ac4ed8d42fe9b6664dc6df039
Oleksandr Riabokon <riabokon.oleksandr@gmail.com>
2025-10-16 16:44:05 +0300

    reset transporter in sendEmail.js

--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -7,14 +7,11 @@ import { getEnvVar } from '../utils/getEnvVar.js';
 
 const transporter = nodemailer.createTransport({
   host: getEnvVar(SMTP.SMTP_HOST),
-  port: Number(getEnvVar(SMTP.SMTP_PORT)),
-  secure: false, //!
+  port: getEnvVar(SMTP.SMTP_PORT),
+  secure: true,
   auth: {
     user: getEnvVar(SMTP.SMTP_USER),
     pass: getEnvVar(SMTP.SMTP_PASSWORD),
-  },
-  tls: {
-    rejectUnauthorized: false, //!
   },
 });
 
